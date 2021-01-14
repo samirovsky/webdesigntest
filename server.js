b3e49d1928f3b5033fca9775188d300029acb480
Samir TABIB <tbib.samir@gmail.com>
2021-01-14 02:30:56 +0100

    Fix cors

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
+
+var whitelist = ['https://webdesign-backend.herokuapp.com/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 app.use(express.static("./dist"));
-app.get("/*", function(req, res) {
+app.get("/*", cors(corsOptions), function(req, res) {
     res.sendFile("index.html", {root: "dist/"}
   );
 });
