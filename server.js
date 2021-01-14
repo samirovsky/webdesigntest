c41694c9124fa5f16dcd42ec3f6fba8c3acee5ce
Samir TABIB <tbib.samir@gmail.com>
2021-01-14 03:18:27 +0100

    Configure build

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 const express = require('express');
-var cors = require('cors');
+// var cors = require('cors');
 const app = express();
 
-var whitelist = ['https://webdesign-backend.herokuapp.com/']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// var whitelist = ['https://webdesign-backend.herokuapp.com/']
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
 app.use(express.static("./dist"));
 
-app.options('/*', cors()) // enable pre-flight request for DELETE request
+// app.options('/*', cors()) // enable pre-flight request for DELETE request
 
-app.del('/*', cors(), function (req, res, next) {
-  res.sendFile("index.html", {root: "dist/"});
-});
+// app.del('/*', cors(), function (req, res, next) {
+//   res.sendFile("index.html", {root: "dist/"});
+// });
 
-app.patch('/*', cors(), function (req, res, next) {
-  res.sendFile("index.html", {root: "dist/"});
-});
+// app.patch('/*', cors(), function (req, res, next) {
+//   res.sendFile("index.html", {root: "dist/"});
+// });
 
-app.post('/*', cors(), function (req, res, next) {
-  res.sendFile("index.html", {root: "dist/"});
-});
+// app.post('/*', cors(), function (req, res, next) {
+//   res.sendFile("index.html", {root: "dist/"});
+// });
 
-app.get("/*", cors(), function(req, res, next) {
+app.get("/*", function(req, res) {
     res.sendFile("index.html", {root: "dist/"}
   );
 });
